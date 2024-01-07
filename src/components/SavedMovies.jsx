3cd62c809afe04ad129e63503936bbae9b611681
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-07 19:07:27 +0100

    fixed design error in saved movies page

--- src/components/SavedMovies.jsx
+++ src/components/SavedMovies.jsx
@@ -9,17 +9,19 @@ const SavedMovies = () => {
 
     return (
       <div
-       className={`${items.length === 0 ? 'h-screen' : 'h-auto'} grid grid-cols-1 md:grid-cols-3 place-items-center gap-10 py-10 px-5 md:px-14`}>
+       className={`${items.length === 0 ? 'h-screen' : 'h-auto grid grid-cols-1 md:grid-cols-3'}  place-items-center gap-10 py-10 px-5 md:px-14`}>
             { items.length > 0
              ?
-              items.map((item, index) => (
+              items.map(item => (
                 <Link to={`/search/${item.id}`} key={item.id}>
                   <MovieCard title={item.name} poster_path={item.image} vote_average={item.rating} />
                 </Link>
               ))
             :
-            <p>There are no saved movies</p>
-            }
+          <p className='text-center relative top-1/2 opacity-50'>
+            There are no saved movies
+          </p>
+        }
       </div>
     );
 }
